b923d4c807b85e4b083d97aa9451fdac447b700e
1Vasilisa1 <153769185+1Vasilisa1@users.noreply.github.com>
2023-12-14 00:57:18 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <windows.h>
+#include <vector>
+
+std::vector<char> ReadRAR4File(const std::string& filename)
+{
+    HANDLE hFile = CreateFileA(filename.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        std::cerr << "Failed to open file." << std::endl;
+        return {};
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    std::vector<char> buffer(fileSize);
+
+    DWORD bytesRead;
+    if (!ReadFile(hFile, buffer.data(), fileSize, &bytesRead, NULL))
+    {
+        std::cerr << "Failed to read file." << std::endl;
+        CloseHandle(hFile);
+        return {};
+    }
+
+    CloseHandle(hFile);
+
+    return buffer;
+}
+
+void PrintBufferContents(const std::vector<char>& buffer)
+{
+    for (char c : buffer)
+    {
+        std::cout << c;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string filename = "C:\\Example.rar";
+
+    std::vector<char> rarData = ReadRAR4File(filename);
+
+    PrintBufferContents(rarData);
+
+    return 0;
+}
